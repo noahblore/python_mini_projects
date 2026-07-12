dd497f3d5ff2097bc7342a250d4ce55a2d704415
Noah Blore <noah.blore@gmail.com>
2026-07-12 15:17:29 +0100

    Finished mini project of Quiz_Game

---
+++ Quiz_Game/Quiz_Game.py
@@ -0,0 +1,45 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? (Yes/No): ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play: :)")
+
+score = 0
+answer = input("What does CPU stand for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print(f"You got {score} questions correct!")
+percent_correct = score/4 *100
+print(f"You got {percent_correct}%")
